eaf215f136043708f2b51facfb37763984c7701f
samuelj1323 <samuelj1323@gmail.com>
2026-09-19 15:31:31 -0400

    preload call

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,12 @@
 <svelte:head>
     <link rel="icon" href={favicon} />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        rel="preload"
+        as="style"
+        href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&family=Schibsted+Grotesk:wght@400..900&family=Special+Elite&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+    />
     <link
         href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&family=Schibsted+Grotesk:wght@400..900&family=Special+Elite&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
         rel="stylesheet"
